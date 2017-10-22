e102977d118c27af083f4fb5703f70b1708ad2a1
yanyun bi <yb83@afsconnect2.njit.edu>
2017-10-22 12:46:30 -0400

    add display.php

---
+++ display.php
@@ -0,0 +1,30 @@
+<?php
+
+class  display extends page
+{
+
+	public function get()
+	{
+	echo "<html><body><table>\n\n"
+	
+	$fh = fopen("./project1/".$_GET['filename']', 'r');
+	while (($line = fgetcsv($fh))!==flase){
+	$row=$line[0];
+	$cells=explode(" ",$row);
+	echo"<tr>";
+	foreach($cell as $cell){
+		echo "<td>" . htmlspecialchars($cell). "</td>";
+
+	}
+	echo "</tr>\n";
+}
+fclose($fh);
+echo "\n</table></body></html>";
+}
+}
+
+?>
+
+        print '</table>';
+
+?>
