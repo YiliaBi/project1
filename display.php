82cf77107d2278bb24bb420656cf570c8eeca6dc
Yanyun Bi <31936712+YiliaBi@users.noreply.github.com>
2017-11-01 14:32:07 -0400

    Update display.php

--- display.php
+++ display.php
@@ -1,46 +1,26 @@
 <?php
 
-class display extends page
+class display1 extends page
 {
+public function get(){
 
-	public function get()
-	{
-	$row = 1;
-	if (($handle = fopen("./upload/".$_GET['filename'], "r")) !== FALSE) {
-	    
-	    echo '<table border="1">';
-		    
-	while (($data = fgetcsv($handle)) !== FALSE) {
-		$num = count($data);
-        	if ($row == 1) {
-	            echo '<thead><tr>';
-		}else{
-		    echo '<tr>';
-		}
-        	for ($c=0; $c < $num; $c++) {
-	             if(empty($data[$c])) {
-			$value = "&nbsp;";
-		     }else{
-		     $value = $data[$c];
-		     }
-		     if ($row == 1) {
-                	echo '<th>'.$value.'</th>';
-		     }else{
-			echo '<td>'.$value.'</td>';
-		     }
-		}
-		if ($row == 1) {
-            		echo '</tr></thead><tbody>';
-	        }else{
-		        echo '</tr>';
-		}
-		$row++;
-	}
-						        
-	echo '</tbody></table>';
-	fclose($handle);
-	}
-	}	
+$f = fopen("./upload/".$_GET['filename'], "r");
+          
+echo '<table border="1">';
+echo '<thead>';
+         
+while (($line = fgetcsv($f)) !== false) {
+        
+        //$cell = explode(" ",$row);
+        echo "<tr>";
+        foreach ($line as $cell) {
+        echo "<td>" . htmlspecialchars($cell) ."</td>";
+        }
+        echo "</tr>\n";
+        }
+fclose($f); 
+echo "\n</table></body></html>";
+}   
 }
 
 ?>
